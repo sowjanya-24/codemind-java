c9fca372255e5dcd15d6d7db95ab3067fff69243
sowjanya-24 <84307382+sowjanya-24@users.noreply.github.com>
2022-09-23 23:41:53 +0530

    import java.util.Scanner; class Primes {     public static boolean isprime(int n)     {         int count=0;         for(int i=2; i<=Math.sqrt(n); i++)         {             if(n%i==0)             count++;         }         if(count==0)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c=0;         a=sc.nextInt();         b=sc.nextInt();         if(a==1)             a=2;         for(int i=a; i<=b; i++)         {             if(isprime(i))             c++;         }         System.out.println(c);     } }

--- Primes_in_range.java
+++ Primes_in_range.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
-class Primeinrange
+class Primes
 {
- public static boolean isprime(int n)
- {
-  int count=0;
-  for(int i=2; i<=(int)Math.sqrt(n); i++)
-  {
-   if(n%i==0)
-     count++;
-  }
-  if(count==0)
-    return true;
-  else
-    return false;
- }
- public static void main(String args[])
- {
-  int a,b,c=0;
-  Scanner sc=new Scanner(System.in);
-  a=sc.nextInt();
-  b=sc.nextInt();
-  if(a==1)
-    a=2;
-  for(int i=a; i<=b; i++)
-  {
-   if (isprime(i))
-       c++;
-  }
-  System.out.println(c);
- }
+    public static boolean isprime(int n)
+    {
+        int count=0;
+        for(int i=2; i<=Math.sqrt(n); i++)
+        {
+            if(n%i==0)
+            count++;
+        }
+        if(count==0)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a==1)
+            a=2;
+        for(int i=a; i<=b; i++)
+        {
+            if(isprime(i))
+            c++;
+        }
+        System.out.println(c);
+    }
 }
