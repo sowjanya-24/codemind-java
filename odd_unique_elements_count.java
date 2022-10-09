3d046cfe6f5ef53d2ad3694facb3318a96fbeb91
sowjanya-24 <84307382+sowjanya-24@users.noreply.github.com>
2022-10-09 22:05:01 +0530

    import java.util.Scanner; class Unique {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,j,count=0,sum=0;         n=sc.nextInt();         x=new int[n];         for(i=0; i<n; i++)         x[i]=sc.nextInt();         for(i=0; i<n; i++)         {             if(x[i]!=-999)             {                 for(j=0; j<n; j++)                 {                     if(x[i]==x[j]&&i!=j)                     {                         x[j]=-999;                     }                 }                 if(x[i]%2!=0)                 {                     count++;                 }             }         }         System.out.println(count);     } }

---
+++ odd_unique_elements_count.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Unique
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,j,count=0,sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0; i<n; i++)
+        x[i]=sc.nextInt();
+        for(i=0; i<n; i++)
+        {
+            if(x[i]!=-999)
+            {
+                for(j=0; j<n; j++)
+                {
+                    if(x[i]==x[j]&&i!=j)
+                    {
+                        x[j]=-999;
+                    }
+                }
+                if(x[i]%2!=0)
+                {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
