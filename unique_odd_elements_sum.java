ab321092838b2400e1b61cbe8d7dd94e33e4c360
sowjanya-24 <84307382+sowjanya-24@users.noreply.github.com>
2022-11-06 18:14:39 +0530

    import java.util.Scanner; class Unique {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],sum=0;         n=sc.nextInt();         x=new int[n];         for(int i=0; i<n; i++)         x[i]=sc.nextInt();         for(int i=0; i<n; i++)         {           if(x[i]!=-999)           {               for(int j=0; j<n; j++)               {                   if(x[i]==x[j]&&i!=j)                   {                       x[j]=-999;                   }               }               if(x[i]%2!=0)               {                   sum=sum+x[i];               }           }         }         System.out.println(sum);     } }

---
+++ unique_odd_elements_sum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Unique
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0; i<n; i++)
+        x[i]=sc.nextInt();
+        for(int i=0; i<n; i++)
+        {
+          if(x[i]!=-999)
+          {
+              for(int j=0; j<n; j++)
+              {
+                  if(x[i]==x[j]&&i!=j)
+                  {
+                      x[j]=-999;
+                  }
+              }
+              if(x[i]%2!=0)
+              {
+                  sum=sum+x[i];
+              }
+          }
+        }
+        System.out.println(sum);
+    }
+}
